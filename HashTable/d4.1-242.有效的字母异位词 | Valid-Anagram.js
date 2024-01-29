98e4b8b935e35c387ee995dc189f2dd0704db3a9
ruanm <247987314@qq.com>
2024-01-29 12:03:41 +0800

    添加 242.有效的字母异位词

---
+++ HashTable/d4.1-242.有效的字母异位词 | Valid-Anagram.js
@@ -0,0 +1,37 @@
+// https://leetcode.cn/problems/valid-anagram/description/
+
+/**
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+    注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+    s 和 t 仅包含小写字母.
+
+    示例 1:
+    输入: s = "anagram", t = "nagaram"
+    输出: true
+
+    示例 2:
+    输入: s = "rat", t = "car"
+    输出: false
+ */
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) return false;
+  let strLen = new Map();
+  for (let str of s) {
+    strLen.set(str, (strLen.get(str) || 0) + 1);
+  }
+  for (let str of t) {
+    // 注意：.get() -> 0 时 !.get() -> true ;
+    //      .get() -> -1 时 !.get() -> false;
+    if (!strLen.get(str)) return false;
+    strLen.set(str, strLen.get(str) - 1);
+  }
+  return true;
+};
+// isAnagram("anagram", "nagaram");
+isAnagram("rat", "car");
